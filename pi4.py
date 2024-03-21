cd6fee423c73bc910efea8765fc5ddc44951aa84
Mateusz0022 <114878437+Mateusz0022@users.noreply.github.com>
2024-03-21 17:28:37 +0100

    Add files via upload

---
+++ pi4.py
@@ -0,0 +1,10 @@
+from mpmath import mp
+
+# Ustaw precyzję na 1000 miejsc po przecinku
+mp.dps = 100000
+
+# Wykonaj obliczenia dla liczby pi
+pi_value = mp.pi
+
+# Wydrukuj wartość π z dokładnością do 1000 miejsc po przecinku
+print(pi_value)
